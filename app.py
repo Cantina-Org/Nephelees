06dca4ea387aa1f734399c3529926d2c3fe5b8d0
mathieu <degueurce.mathieu@gmail.com>
2022-12-18 18:26:29 +0100

    GitIgnore modif dans App.py et d'autres truc

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request, url_for, redirect, make_respo
 import mariadb
 import hashlib
 import os
-import uuid
+# import uuid
 
 
 def hash_perso(passwordtohash):
@@ -18,23 +18,26 @@ def hash_perso(passwordtohash):
     passw = hashlib.md5(passw).hexdigest()
     return passw
 
-con = mariadb.connect(user="mathieu", password="LeMdPDeTest", host="localhost", port=3306, database="cantina_db")
+
+con = mariadb.connect(user="cantina", password="LeMdPDeTest", host="localhost", port=3306, database="cantina_db")
 cursor = con.cursor()
 cursor.execute("CREATE TABLE IF NOT EXISTS user(ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT, token TEXT, "
-           "user_name TEXT, password TEXT, admin BOOL, online BOOL, last_online TEXT)")
+               "user_name TEXT, password TEXT, admin BOOL, online BOOL, last_online TEXT)")
 
-# cursor.execute(f"""INSERT INTO user(token, user_name, password, admin) VALUES ({uuid.uuid3(uuid.uuid1(), str(uuid.uuid1()))}, 'matbe3', '{hash_perso("")}', 0)""")
+# cursor.execute(f"""INSERT INTO user(token, user_name, password, admin) VALUES ('{uuid.uuid3(uuid.uuid1(),
+# str(uuid.uuid1()))}', 'matbe2', '{hash_perso("Asvel2021_._")}', 0)""")
 
 con.commit()
 
 path2, filenames, lastPath = "", "", ""
-dir_path = "/home/mathieu/Bureau/cantina/file_cloud"
+dir_path = os.path.abspath(os.getcwd())+'/file_cloud'
 app = Flask(__name__)
 app.config['UPLOAD_PATH'] = dir_path
 
+
 @app.route('/')
 def hello_world():  # put application's code here
-    cursor.execute('''SELECT user_name FROM user WHERE token = ?''', (request.cookies.get('userID'),))
+    cursor.execute('''SELECT user_name, admin FROM user WHERE token = ?''', (request.cookies.get('userID'),))
     return render_template('home.html', cur=cursor.fetchall())
 
 
@@ -42,7 +45,7 @@ def hello_world():  # put application's code here
 def file():
     global path2, filenames, lastPath
     userToken = request.cookies.get('userID')
-    cursor.execute(f'''SELECT token FROM user WHERE admin''',)
+    cursor.execute(f'''SELECT token FROM user WHERE admin''')
     row = cursor.fetchall()
 
     if not [tup for tup in row if userToken in tup]:
@@ -51,7 +54,6 @@ def file():
     args = request.args
     lastPath = ""
     work_file_in_dir, work_dir = [], []
-
 
     if not args.getlist('path'):
         for (dirpath, dirnames, filenames) in walk(dir_path):
@@ -62,8 +64,8 @@ def file():
     else:
         last_path_1 = args.get('path')
         last_path_1 = last_path_1[:-1].split("/")
-        for i in range(0, len(last_path_1)-1):
-            lastPath = lastPath + last_path_1[i]+'/'
+        for i in range(0, len(last_path_1) - 1):
+            lastPath = lastPath + last_path_1[i] + '/'
 
         for (dirpath, dirnames, filenames) in walk(dir_path + args.get('path')):
             work_file_in_dir.extend(filenames)
@@ -79,25 +81,24 @@ def file():
     elif args.get('path') == "/":
         path2 = args.get('path')
 
-
     if not args.get('action') or args.get('action') == 'show':
         return render_template('myfile.html', dir=work_dir, file=work_file_in_dir, path=path2, lastPath=lastPath)
 
     elif args.get('action') == "deleteFile" and args.get('workFile') and args.get('workFile') in filenames:
-        os.remove(dir_path+path2+args.get('workFile'))
+        os.remove(dir_path + path2 + args.get('workFile'))
         return render_template("redirect/r-myfile.html", path="/my/file/?path=/", lastPath=lastPath)
 
     elif args.get('action') == "createFile" and args.get('workFile'):
-        fd = os.open(dir_path+path2+args.get('workFile'), os.O_RDWR | os.O_CREAT)
+        fd = os.open(dir_path + path2 + args.get('workFile'), os.O_RDWR | os.O_CREAT)
         os.close(fd)
         return render_template("redirect/r-myfile.html", path="/my/file/?path=/", lastPath=lastPath)
 
     elif args.get('action') == "deleteFolder" and args.get('workFile') and args.get('workFile') in filenames:
-        os.rmdir(dir_path+path2+args.get('workFile'))
+        os.rmdir(dir_path + path2 + args.get('workFile'))
         return render_template("redirect/r-myfile.html", path="/my/file/?path=/", lastPath=lastPath)
 
     elif args.get('action') == "createFolder" and args.get('workFile'):
-        os.mkdir(dir_path+path2+args.get('workFile'))
+        os.mkdir(dir_path + path2 + args.get('workFile'))
         return render_template("redirect/r-myfile.html", path="/my/file/?path=/", lastPath=lastPath)
 
     else:
@@ -106,11 +107,11 @@ def file():
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
-    
     if request.method == 'POST':
         user = request.form['nm']
         passwd = request.form['passwd']
-        cursor.execute(f'''SELECT user_name, password, token FROM user WHERE password = ? AND user_name = ?''', (hash_perso(passwd), user))
+        cursor.execute(f'''SELECT user_name, password, token FROM user WHERE password = ? AND user_name = ?''',
+                       (hash_perso(passwd), user))
         row = cursor.fetchone()
 
         try:
@@ -118,9 +119,9 @@ def login():
                 resp = make_response(redirect(url_for('hello_world')))
                 resp.set_cookie('userID', row[2])
                 return resp
-        except:
+        except Exception as e:
+            print(e)
             return redirect(url_for("hello_world"))
-
 
     elif request.method == 'GET':
         return render_template('login.html')
@@ -142,7 +143,7 @@ def upload_file():
             return redirect(url_for('hello_world'))
 
         f = request.files['file']
-        f.save(os.path.join(dir_path+args.get('path'), secure_filename(f.filename)))
+        f.save(os.path.join(dir_path + args.get('path'), secure_filename(f.filename)))
         return redirect(url_for('file', path=args.get('path')))
 
 
@@ -157,7 +158,7 @@ def download_file():
     if not [tup for tup in row if userToken in tup]:
         return redirect(url_for('hello_world'))
 
-    return send_from_directory(directory=dir_path+args.get('path'), path=args.get('item'))
+    return send_from_directory(directory=dir_path + args.get('path'), path=args.get('item'))
 
 
 if __name__ == '__main__':
