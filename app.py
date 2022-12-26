7112409896360b69708b5ce3c0f80d6475085978
mathieu <degueurce.mathieu@gmail.com>
2022-12-26 11:20:44 +0100

    -début de la modif du système de fichier - le createFolder marche (presque)

--- app.py
+++ app.py
@@ -62,42 +62,44 @@ def home():  # put application's code here
 @app.route('/my/file/')
 def file():
     global path2, filenames, lastPath
-    user_token = request.cookies.get('userID')
-    cursor.execute(f'''SELECT token FROM user WHERE admin''')
-    row = cursor.fetchall()
-
-    if not [tup for tup in row if user_token in tup]:
-        return redirect(url_for('home'))
-
     args = request.args
-    lastPath = ""
     work_file_in_dir, work_dir = [], []
+    user_token = request.cookies.get('userID')
+
+    cursor.execute(f'''SELECT work_Dir, admin FROM user WHERE token = ?''', (user_token,))
+    row = cursor.fetchone()
 
     if not args.getlist('path'):
-        for (dirpath, dirnames, filenames) in walk(dir_path):
-            work_file_in_dir.extend(filenames)
-            work_dir.extend(dirnames)
-            break
+        if row[1]:
+            for (dirpath, dirnames, filenames) in walk(dir_path):
+                work_file_in_dir.extend(filenames)
+                work_dir.extend(dirnames)
+                break
+        elif not row[1]:
+            print(row)
+            for (dirpath, dirnames, filenames) in walk(row[0]):
+                work_file_in_dir.extend(filenames)
+                work_dir.extend(dirnames)
+                break
 
     else:
         last_path_1 = args.get('path')
         last_path_1 = last_path_1[:-1].split("/")
+        print(last_path_1)
         for i in range(0, len(last_path_1) - 1):
             lastPath = lastPath + last_path_1[i] + '/'
-
-        for (dirpath, dirnames, filenames) in walk(dir_path + args.get('path')):
-            work_file_in_dir.extend(filenames)
-            work_dir.extend(dirnames)
-            break
-
-    if not args.get('path'):
-        path2 = "/"
-
-    elif args.get('path') != "/":
-        path2 = args.get('path') + "/"
-
-    elif args.get('path') == "/":
-        path2 = args.get('path')
+        print(lastPath)
+        if row[1]:
+            for (dirpath, dirnames, filenames) in walk(dir_path + '/' + args.get('path')):
+                work_file_in_dir.extend(filenames)
+                work_dir.extend(dirnames)
+                break
+            print(dir_path + args.get('path'))
+        elif not row[1]:
+            for (dirpath, dirnames, filenames) in walk(row[0] + args.get('path')):
+                work_file_in_dir.extend(filenames)
+                work_dir.extend(dirnames)
+                break
 
     if not args.get('action') or args.get('action') == 'show':
         return render_template('myfile.html', dir=work_dir, file=work_file_in_dir, path=path2, lastPath=lastPath)
@@ -116,7 +118,7 @@ def file():
         return render_template("redirect/r-myfile.html", path="/my/file/?path=/", lastPath=lastPath)
 
     elif args.get('action') == "createFolder" and args.get('workFile'):
-        os.mkdir(dir_path + path2 + args.get('workFile'))
+        os.mkdir(dir_path + path2 + '/' + args.get('workFile'))
         return render_template("redirect/r-myfile.html", path="/my/file/?path=/", lastPath=lastPath)
 
     else:
@@ -180,7 +182,7 @@ def download_file():
         return redirect(url_for('home'))
 
     make_log('Download file', request.remote_addr, request.cookies.get('userID'), 1,
-             dir_path+args.get('path')+args.get('item'))
+             dir_path + args.get('path') + args.get('item'))
     return send_from_directory(directory=dir_path + args.get('path'), path=args.get('item'))
 
 
@@ -249,11 +251,11 @@ def admin_add_user():
                         print(e)
                         admin = False
                     newUUID = str(uuid.uuid3(uuid.uuid1(), str(uuid.uuid1())))
-                    cursor.execute('''INSERT INTO user(token, user_name, password, admin) VALUES (?, ?, ?, ?)''', (
-                        newUUID, request.form['uname'],
-                        hash_perso(request.form['pword2']), admin))
+                    cursor.execute('''INSERT INTO user(token, user_name, password, admin, work_Dir) VALUES (?, ?, ?, 
+                            ?, ?)''', (newUUID, request.form['uname'], hash_perso(request.form['pword2']), admin,
+                                       dir_path + '/' + secure_filename(request.form['uname'])))
                     con.commit()
-                    os.mkdir(dir_path+'/'+secure_filename(request.form['uname']))
+                    os.mkdir(dir_path + '/' + secure_filename(request.form['uname']))
                     make_log('add_user', request.remote_addr, request.cookies.get('userID'), 2,
                              'Created user token: ' + newUUID)
                     return redirect(url_for('admin_user_manager'))
