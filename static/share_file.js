965432352fcf2e2734e065af20e9fb645378e364
mathieu <degueurce.mathieu@gmail.com>
2022-12-28 23:21:45 +0100

    Ajout de la catégorie 'Show Share Files' dans le panel ADMIN avec possibilité de supprimer le fichier.

--- static/share_file.js
+++ static/share_file.js
@@ -1,4 +1,13 @@
+function updateClipboard(newClip) {
+  navigator.clipboard.writeText(newClip).then(function() {
+    console.log('All is fine!')
+  }, function() {
+    console.log('Fail Paste To CopyBoard!')
+  });
+}
+
 function share_file(workDir, file_to_share){
+    
     if (confirm("Voulez vous partager ce fichier avec toute les personnes qui ont le lien? ")) {
         window.location.replace("?path="+workDir+"&action=shareFile&workFile="+file_to_share+"&loginToShow=0");
     } else {
