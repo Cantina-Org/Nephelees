546c07380ba406c0ad55cf96bf73bb040c4e7054
mathieu <degueurce.mathieu@gmail.com>
2023-01-31 21:36:31 +0100

    Ajout du pull d'un repos github

--- static/git-js/clone_repository.js
+++ static/git-js/clone_repository.js
@@ -1,4 +1,4 @@
 function clone_repository(workDir){
-    let repoLink = prompt("Quelle est le lien du dépots Github? ")
+    let repoLink = prompt("Quelle est le lien du dépots distants? ")
     window.location.replace("?path="+workDir+"&action=cloneRepo&repoLink="+repoLink);
 }
