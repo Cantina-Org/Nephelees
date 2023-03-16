573f0aadbaa6e1c8a1d30bc043a7a33b6b42db23
mathieu <degueurce.mathieu@gmail.com>
2023-03-16 11:31:20 +0100

    je sais plus

--- app.py
+++ app.py
@@ -680,7 +680,7 @@ def add_user_api():
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('error/404.html'), 404
+    return render_template('error/404.html'), error
 
 
 if __name__ == '__main__':
