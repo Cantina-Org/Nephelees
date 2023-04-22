c438d430425d7acd7117a1646a7b502e36b72ecd
matyu <degueurce.mathieu@gmail.com>
2023-04-22 19:13:38 +0200

    - Suppression des fonctions qui vont partir sur Olympie

--- app.py
+++ app.py
@@ -105,53 +105,6 @@ def login():
 def logout():
     return logout_cogs(request, database)
 
-
-# Fonction permettant de voire la page 'principale' du panel Admin de Cantina Cloud 
-@app.route('/admin/home')
-def admin_home():
-    return home_admin_cogs(request, database, dir_path)
-
-
-# Fonction permettant de visualiser les utilisateur de Cantina Cloud
-@app.route('/admin/usermanager/')
-@app.route('/admin/usermanager/<user_name>')
-def admin_show_user(user_name=None):
-    return show_user_cogs(request, database, user_name)
-
-
-# Fonction permettant de créer un utilisateur
-@app.route('/admin/add_user/', methods=['POST', 'GET'])
-def admin_add_user():
-    return add_user_cogs(request, database, dir_path, share_path)
-
-
-# Fonction permettant de voire les logs générer par Cantina Cloud
-@app.route('/admin/show_log/')
-@app.route('/admin/show_log/<log_id>')
-def admin_show_log(log_id=None):
-    return show_log_cogs(request, database, log_id)
-
-
-# Fonction permettant de voire tout les fichiers partagé
-@app.route('/admin/show_share_file/')
-@app.route('/admin/show_share_file/<random_name>')
-def admin_show_share_file(random_name=None):
-    return show_share_file_cogs(request, database, share_path, random_name)
-
-
-# Fonction permettant de voire les API créer sur Cantina Cloud
-@app.route('/admin/api_manager/')
-@app.route('/admin/api_manager/<api_id>')
-def admin_api_manager(api_id=None):
-    return api_manager_cogs(request, database, api_id)
-
-
-# Fonction permettant de créer une API Cantina Cloud
-@app.route('/admin/add_api/', methods=['POST', 'GET'])
-def admin_add_api():
-    return add_api_cogs(request, database)
-
-
 ###########################################
 #                  API                    #
 ###########################################
